eb6a949ba4a23b684666144981d244cdd225822b
Deepak Sharma <dk.sharma@wassup.co.in>
2025-12-03 23:02:18 +0530

    killer

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
           <div className="w-10 h-10 rounded-lg bg-primary flex items-center justify-center font-mono font-bold text-primary-foreground">
             DS
           </div>
-          <span className="font-bold text-xl hidden sm:inline">Shiv gham Kumar</span>
+          <span className="font-bold text-xl hidden sm:inline">Shiv plato Kumar</span>
         </Link>
 
         <nav className="hidden md:flex items-center gap-8">
